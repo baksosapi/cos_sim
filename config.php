d105e096bffd9fbadb4b2e6a2c61c208b8b76798
wildan.mbp <wildan.ubg@gmail.com>
2016-10-16 15:49:15 +0800

    edit bootstrap

--- config.php
+++ config.php
@@ -3,17 +3,17 @@
  * Created by PhpStorm.
  * User: wildan
  * Date: 9/29/16
- * Time: 11:26 AM
+ * Time: 11:26 AM.
  */
 
 define('LIBRARY', 'lib/');
 define('BASE_URL', 'http://localhost/kat/');
 define('ROOT_DIR', dirname(__FILE__));
 
-define('DB_VENDOR','mysql');
-define('DB_HOST','localhost');
-define('DB_NAME','perpusku');
-define('DB_USER','root');
-define('DB_PASSWD','');
+define('DB_VENDOR', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'perpusku');
+define('DB_USER', 'root');
+define('DB_PASSWD', '');
 
-define('LOG_PATH',ROOT_DIR.'/logs/');
+define('LOG_PATH', ROOT_DIR.'/logs/');
--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once ("config.php");
+require_once 'config.php';
 
 spl_autoload_register(function ($class) {
-    include 'src'.str_replace('\\', DIRECTORY_SEPARATOR, '/') . str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+    include 'src'.str_replace('\\', DIRECTORY_SEPARATOR, '/').str_replace('\\', DIRECTORY_SEPARATOR, $class).'.php';
 });
 
+// Call Bootstrap
 $app = new Bootstrap();
